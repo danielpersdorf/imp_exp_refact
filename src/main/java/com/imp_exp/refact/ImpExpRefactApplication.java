099d5b6bbd70ff659be24d4c993fc1e909a8f865
danielpersdorf <daniel.persdorf@ipas.de>
2022-04-03 13:17:58 +0200

    Initial commit

---
+++ src/main/java/com/imp_exp/refact/ImpExpRefactApplication.java
@@ -0,0 +1,16 @@
+package com.imp_exp.refact;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ImpExpRefactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpExpRefactApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/imp_exp/refact/ImpExpRefactApplicationTests.java
@@ -0,0 +1,14 @@
+package com.imp_exp.refact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpExpRefactApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
